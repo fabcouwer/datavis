8b3b27278aed7871923754c4f186575c29d33dec
Friso Abcouwer <f.abcouwer@gmail.com>
2014-12-13 16:59:01 +0100

    Transform data to desired input for D3. Filewrite NYI

---
+++ Assignment_1/Data Extraction/Java implementation/DataExtraction/src/OpeningStrategy.java
@@ -0,0 +1,113 @@
+package datavis;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class OpeningStrategy {
+
+	// Name and overall wins, losses and winrate
+	private String name;
+	private int totalWins;
+	private int totalLosses;
+	private double overallWinrate;
+
+	// 3 hashmaps to keep track of wins, losses and winrate vs. every opponent
+	private HashMap<String, Integer> wins;
+	private HashMap<String, Integer> losses;
+	private HashMap<String, Double> winrates;
+
+	public OpeningStrategy(String name) {
+		this.name = name;
+		this.totalWins = 0;
+		this.totalLosses = 0;
+		this.overallWinrate = 0d;
+		this.wins = new HashMap<String, Integer>();
+		this.losses = new HashMap<String, Integer>();
+		this.winrates = new HashMap<String, Double>();
+	}
+
+	// Increment win/loss counters
+	public void incrementWins(String opponentName, String tier) {
+		this.totalWins += 1;
+
+		if (this.wins.get(opponentName) == null) {
+			this.wins.put(opponentName, 0);
+		}
+		if (this.wins.get(tier) == null) {
+			this.wins.put(tier, 0);
+		}
+		this.wins.put(opponentName, this.wins.get(opponentName) + 1);
+		this.wins.put(tier, this.wins.get(tier) + 1);
+	}
+
+	public void incrementLosses(String opponentName, String tier) {
+		this.totalLosses += 1;
+
+		if (this.losses.get(opponentName) == null) {
+			this.losses.put(opponentName, 0);
+		}
+		if (this.losses.get(tier) == null) {
+			this.losses.put(tier, 0);
+		}
+		this.losses.put(opponentName, this.losses.get(opponentName) + 1);
+		this.losses.put(tier, this.losses.get(tier) + 1);
+	}
+
+	// Update total winrate
+	public void updateTotalWinrate() {
+		this.overallWinrate = (double) this.totalWins
+				/ (this.totalLosses + this.totalWins);
+	}
+
+	// Update all individual winrates
+	public void updateWinrateHashmaps() {
+
+		// Get names of all opponents
+		Set<String> keys = new HashSet<String>(wins.keySet());
+		keys.addAll(losses.keySet());
+
+		ArrayList<String> opponents = new ArrayList<String>();
+		opponents.addAll(keys);
+
+		for (String opponent : opponents) {
+			int w, l;
+
+			if (wins.get(opponent) == null) {
+				w = 0;
+				wins.put(opponent, 0);
+			} else {
+				w = wins.get(opponent);
+			}
+			if (losses.get(opponent) == null) {
+				l = 0;
+				losses.put(opponent, 0);
+			} else {
+				l = losses.get(opponent);
+			}
+			winrates.put(opponent, (double) w / (w + l));
+		}
+	}
+
+	// Returns a CSV-ready String with overall stats.
+	// Example: "2t1m1b,3,3,0.5"
+	public String toStringOverall() {
+		return (this.name + "," + this.totalWins + "," + this.totalLosses + "," + this.overallWinrate);
+	}
+
+	// Returns a CSV-ready String with individual statistics.
+	// Example: "A,B,3,3,0.5\nA,C,4,1,0.8\n"
+	public String toStringIndividual() {
+		String s = "";
+		for (HashMap.Entry<String, Double> entry : winrates.entrySet()) {
+			String opponent = entry.getKey();
+			s += this.name + "," + entry.getKey() + "," + wins.get(opponent)
+					+ "," + losses.get(opponent) + "," + entry.getValue()
+					+ "\n";
+		}
+		return s;
+	}
+
+}
---
+++ Assignment_1/Data Extraction/Java implementation/DataExtraction/src/WinRateExtractor.java
@@ -0,0 +1,110 @@
+package datavis;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.HashMap;
+
+//Reads a file with opening strategies and outputs:
+// - A file with winrates for every distinct opening strategy 
+// - A file for every strategy's performance against other strategies
+public class WinRateExtractor {
+
+	public static String fileName = "C:\\Users\\F\\Downloads\\5minuteOpeningStrat.csv";
+	public static HashMap<String, OpeningStrategy> strats;
+
+	public static void main(String[] args) {
+		strats = new HashMap<String, OpeningStrategy>();
+
+		// Read file
+		BufferedReader br = null;
+		String line = "";
+		String csvSplitBy = ",";
+		try {
+
+			br = new BufferedReader(new FileReader(fileName));
+			br.readLine();
+			// Extract 2 lines at a time for processing
+			while ((line = br.readLine()) != null) {
+				line = line.replaceAll("[^a-zA-Z,12345678910]", "");
+				// use comma as separator
+				String[] radiant = line.split(csvSplitBy);
+				System.out.println(line);
+				line = br.readLine();
+				System.out.println(line);
+				String[] dire = line.split(csvSplitBy);
+				processMatch(radiant, dire);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Update all opening strategies' winrates
+		for (OpeningStrategy os : strats.values()) {
+			os.updateTotalWinrate();
+			os.updateWinrateHashmaps();
+			System.out.println("Overall Stats:");
+			System.out.println(os.toStringOverall());
+			System.out.println("Individual Stats:");
+			System.out.println(os.toStringIndividual());
+		}
+
+		// Output opening strategies to file
+
+	}
+
+	public static void processMatch(String[] radiant, String[] dire) {
+		// Generate the name string for both strategies
+		String radiantStrategy = extractStrategy(radiant);
+		String direStrategy = extractStrategy(dire);
+
+		// If both teams used the same strategy, handle them as one
+		if (radiantStrategy.equals(direStrategy)) {
+			OpeningStrategy strat = strats.get(radiantStrategy);
+			if (strat == null) {
+				strat = new OpeningStrategy(radiantStrategy);
+			}
+
+			strat.incrementWins(radiantStrategy, radiant[7]);
+			strat.incrementLosses(radiantStrategy, radiant[7]);
+			strats.put(radiantStrategy, strat);
+
+		} else {
+			// See if the strategies are already in the hashmap
+			OpeningStrategy radiantOS = strats.get(radiantStrategy);
+			OpeningStrategy direOS = strats.get(direStrategy);
+
+			if (radiantOS == null) {
+				radiantOS = new OpeningStrategy(radiantStrategy);
+			}
+			if (direOS == null) {
+				direOS = new OpeningStrategy(direStrategy);
+			}
+
+			// Update win/loss
+			if (radiant[6].equals("Radiant")) {
+				// radiant victory
+				radiantOS.incrementWins(direStrategy, radiant[7]);
+				direOS.incrementLosses(radiantStrategy, dire[7]);
+			} else {
+				// dire victory
+				direOS.incrementWins(radiantStrategy, dire[7]);
+				radiantOS.incrementLosses(direStrategy, radiant[7]);
+			}
+
+			// Update strats hashmap
+			strats.put(radiantStrategy, radiantOS);
+			strats.put(direStrategy, direOS);
+		}
+	}
+
+	// Returns a String of the format "NtNbNmNj"
+	public static String extractStrategy(String[] line) {
+		String strategy = "";
+		strategy += line[2] + "t" + line[3] + "b" + line[4] + "m" + line[5]
+				+ "j";
+		return strategy;
+	}
+
+}
